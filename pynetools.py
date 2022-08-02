656a4aad59a7b86a3c02b85df606c1ddb2b75ec3
oliv10 <revi99@me.com>
2022-08-02 20:15:01 +0000

    Add new Tools Dynamically Through Config File

--- pynetools.py
+++ pynetools.py
@@ -6,10 +6,9 @@ except ImportError:
     pass
 import subprocess
 import argparse
-import os
 
 def download(tool: str, path: str) -> subprocess.CompletedProcess:
-    BASE = ['git', 'clone', '-b', 'release']
+    BASE = ['git', 'clone', '-b', 'release'] # -b release : Clones the branch named 'release'
     name = tool.split("/")[-1]
     path = [path+"/"+name]
     url = ["https://www.github.com/"+tool]
@@ -19,61 +18,52 @@ def download(tool: str, path: str) -> subprocess.CompletedProcess:
 def main(args):
     print(TITLE)
 
-    TOOLS = []
-
-    with open("config/TOOLS.conf", "r") as file:
-        config = file.readlines()
-    for tool in config:
-        TOOLS.append(tool.strip())
-
     # Install All Tools
     if args.ALL:
         for tool in TOOLS:
-            download(tool, args.path)
-
-    # Install Selected Tools
-    if not args.ALL:
-        if args.pyne:
-            download('hoplite-consulting/pyne', args.path)
-        if args.vulnage:
-            download('hoplite-consulting/vulnage', args.path)
-        if args.epss:
-            download('hoplite-consulting/epss-cli', args.path)
-        if args.crackpot:
-            download('hoplite-consulting/crackpot-cli', args.path)
+            download(tool, args.PATH)
+        exit()
+    # Dynamically Install Selected Tools
+    else:
+        for tool in TOOLS:
+            t = tool.split("/")[1].replace("-","_")
+            c = getattr(args, t)
+            if c:
+                download(tool, args.PATH)
 
     # Update Tools
-    # if args.Update:
+    # if args.UPDATE:
     #     pass
-    
-    # Install Tools
-    try:
-        os.listdir(args.path)
-    except FileNotFoundError:
-        print("Tools Directory Not Found")
-        exit()
-    for tool in os.listdir(args.path):
-        cmd = ['pip3', 'install', '-r', args.path+"/"+tool+"/requirements.txt"]
-        subprocess.run(cmd)
 
 if __name__ == "__main__":
     
-    __version__ = "1.0.1"
+    __version__ = "2.0.0"
     NAME = "PyneTools"
     DESCRIPTION = "An installer and updater for the collection of tools that work well with the Pyne Nessus Parser."
     try:
         TITLE = pyfiglet.figlet_format(NAME, font="stop") + f"\n{NAME} {__version__}\n\n{DESCRIPTION}\n"
     except:
         TITLE = f"{NAME} {__version__}\n\n{DESCRIPTION}\n"
+    TOOLS = []
 
     parser = argparse.ArgumentParser(description=f"{TITLE}", formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('path', help='path to store tools')
-    # parser.add_argument('-U', '--Update', action='store_true', help='update installed tools') # Need to to more work for this, use list input for specific tools to update? Force all tools to be updated?
+    parser.add_argument('PATH', help='path to store tools')
     parser.add_argument('-A', '--ALL', action='store_true', help='install all tools')
-    parser.add_argument('--pyne', action='store_true', help='install pyne')
-    parser.add_argument('--vulnage', action='store_true', help='install vulnage')
-    parser.add_argument('--epss', action='store_true', help='install epss cli')
-    parser.add_argument('--crackpot', action='store_true', help='install crackpot cli')
+    # parser.add_argument('-U', '--UPDATE', action='store_true', help='update installed tools (this will update ALL tools)') # Need to to more work for this, use list input for specific tools to update? Force all tools to be updated?
+
+    # Get Tools from Config
+    with open("config/TOOLS.conf", "r") as file:
+        config = file.readlines()
+    for tool in config:
+        TOOLS.append(tool.strip())
+
+    # Dynamically Create Install Arguments for each Tool
+    for tool in TOOLS:
+        t = tool.split('/')[-1]
+        a = '--' + t
+        h = 'install ' + t
+        parser.add_argument(a, action='store_true', help=h)
+    
     args = parser.parse_args()
 
     main(args)
